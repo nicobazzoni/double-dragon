769598e17d7429daa76d38428e3a76448ea0c6d1
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2023-10-09 11:20:47 -0400

    yo

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import videoSrc from '../assets/images/fight.mp4';
+import videoSrc from '../assets/images/fight_compressed.mp4';
 
 const VideoPlayer = () => {
     return (
         <div className="video-container">
-            <video controls width="100%" height="auto" autoPlay muted loop>
+            <video  width="100%" height="auto" autoPlay muted loop>
                 <source src={videoSrc} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
