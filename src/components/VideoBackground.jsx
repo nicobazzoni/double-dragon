900090e15540c83af0e9e385309d46e38d2b7153
Nicholas Bazzoni <nico@Nicholass-MBP.fios-router.home>
2023-10-09 09:19:51 -0400

    yo

--- src/components/VideoBackground.jsx
+++ src/components/VideoBackground.jsx
@@ -4,7 +4,7 @@ import videoSrc from '../assets/images/fight.mp4';
 const VideoPlayer = () => {
     return (
         <div className="video-container">
-            <video width="100%" height="auto" autoPlay muted loop>
+            <video controls width="100%" height="auto" autoPlay muted loop>
                 <source src={videoSrc} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
